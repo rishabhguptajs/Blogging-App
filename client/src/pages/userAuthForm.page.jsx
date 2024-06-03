b1b046dd5f6a78cfb171edba537fedda647d98fd
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-06-03 21:55:55 +0530

    worked on signup/login backend

--- client/src/pages/userAuthForm.page.jsx
+++ client/src/pages/userAuthForm.page.jsx
@@ -2,77 +2,80 @@ import React from "react"
 import InputBox from "../components/input.component"
 import googleIcon from "../imgs/google.png"
 import { Link } from "react-router-dom"
+import AnimationWrapper from "../common/page-animation"
 
 const UserAuthForm = ({ type }) => {
   return (
     <div>
-      <section className="h-cover flex items-center justify-center">
-        <form action="" className="w-[80%] max-w-[400px]">
-          <h1 className="text-4xl font-gelasio capitalize text-center">
-            {type === "sign-in" ? "Welcome back!" : "Join us today!"}
-          </h1>
+      <AnimationWrapper keyValue={type}>
+        <section className="h-cover flex items-center justify-center">
+          <form action="" className="w-[80%] max-w-[400px]">
+            <h1 className="text-4xl font-gelasio capitalize text-center">
+              {type === "sign-in" ? "Welcome back!" : "Join us today!"}
+            </h1>
 
-          {type != "sign-in" ? (
+            {type != "sign-in" ? (
+              <InputBox
+                name="fullname"
+                type="text"
+                placeholder="Full Name"
+                icon="fi-rr-user"
+              />
+            ) : (
+              ""
+            )}
             <InputBox
-              name="fullname"
-              type="text"
-              placeholder="Full Name"
-              icon="fi-rr-user"
+              name="email"
+              type="email"
+              placeholder="Email"
+              icon="fi-sr-at"
             />
-          ) : (
-            ""
-          )}
-          <InputBox
-            name="email"
-            type="email"
-            placeholder="Email"
-            icon="fi-sr-at"
-          />
-          <InputBox
-            name="password"
-            type="password"
-            placeholder="Password"
-            icon="fi-rr-key"
-          />
+            <InputBox
+              name="password"
+              type="password"
+              placeholder="Password"
+              icon="fi-rr-key"
+            />
 
-          <button className="btn-dark center mt-14" type="submit">
-            {type.replace("-", " ")}
-          </button>
+            <button className="btn-dark center mt-14" type="submit">
+              {type.replace("-", " ")}
+            </button>
 
-          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-            <hr className="w-1/2 border-black" />
-            <p>or</p>
-            <hr className="w-1/2 border-black" />
-          </div>
+            <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+              <hr className="w-1/2 border-black" />
+              <p>or</p>
+              <hr className="w-1/2 border-black" />
+            </div>
 
-          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-            <img src={googleIcon} alt="" className="w-5" />
-            Continue with Google
-          </button>
+            <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+              <img src={googleIcon} alt="" className="w-5" />
+              Continue with Google
+            </button>
 
-          {type == "sign-in" ? (
-            <p className="mt-6 text-dark-grey text-xl text-center">
-              Don't have an account?
-              <Link
-                to="/signup"
-                className="underline text-black text-xl ml-1"
-              >
-                Join us today!
-              </Link>
-            </p>
-          ) : (
-            <p className="mt-6 text-dark-grey text-xl text-center">
-              Already a member?
-              <Link
-                to="/signin"
-                className="underline text-black text-xl ml-1"
-              >
-                Sign in
-              </Link>
-            </p>
-          )}
-        </form>
-      </section>
+            {type == "sign-in" ? (
+              <p className="mt-6 text-dark-grey text-xl text-center">
+                Don't have an account?
+                <Link
+                  to="/signup"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Join us today!
+                </Link>
+              </p>
+            ) : (
+              <p className="mt-6 text-dark-grey text-xl text-center">
+                Already a member?
+                <Link
+                  to="/signin"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Sign in
+                </Link>
+              </p>
+            )}
+          </form>
+        </section>
+      </AnimationWrapper>
     </div>
   )
 }
---
+++ server/server.js
@@ -0,0 +1,134 @@
+import express, { json } from 'express'
+import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
+import User from './Schema/User.js';
+import connectDB from './config/db.js';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken'
+
+const app = express()
+
+dotenv.config();
+connectDB();
+
+app.use(json())
+
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+
+const generateUsername = async(email) => {
+    const username = email.split('@')[0];
+
+    const checkUser = await User.findOne({ "personal_info.username": username });
+
+    if (checkUser) {
+        return `${username}${nanoid(5)}`
+    }    else {
+        return username;
+    }
+}
+
+const formatDataToSend = (user) => {
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    return {
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname,
+        access_token: token
+    }
+}
+
+app.post('/signup', async(req, res) => {
+    try {
+        const { fullname, email, password } = req.body;
+
+        if (!fullname || !email || !password) {
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        if(fullname.length < 3) {
+            return res.status(403).json({message: "Fullname must be at least 3 characters"})
+        }
+
+        if (!emailRegex.test(email)) {
+            return res.status(403).json({message: "Invalid email"})
+        }
+
+        if (!passwordRegex.test(password)) {
+            return res.status(403).json({message: "Password must contain at least one number and one uppercase and lowercase letter, and at least 6 or more characters"})
+        }
+
+        const checkUser = await User.findOne({ "personal_info.email": email });
+
+        if (checkUser) {
+            return res.status(403).json({message: "User already exists"})
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hash(password, salt);
+
+        let username = await generateUsername(email);
+
+        const user = new User({
+            personal_info: {
+                fullname,
+                email,
+                password: hashedPassword,
+                username
+            }
+        })
+
+        await user.save();
+
+        res.status(200).json({
+            message: "User created successfully",
+            user: formatDataToSend(user)
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating user",
+            error: error.message
+        })
+    }
+})
+
+app.post('/login', async(req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        const user = await User.findOne({ "personal_info.email": email });
+
+        if (!user) {
+            return res.status(403).json({message: "User not found"})
+        }
+
+        const isPasswordValid = bcrypt.compare(password, user.personal_info.password);
+
+        if (!isPasswordValid) {
+            return res.status(403).json({message: "Invalid password"})
+        }
+
+        res.status(200).json({
+            message: "User logged in successfully",
+            user: formatDataToSend(user)
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error logging in",
+            error: error.message
+        })
+    }
+})
+
+const PORT = 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
