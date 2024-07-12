4aebdf1b9954b4b7797046881db483322cbeba1b
rishabhguptajs <rishabhgupta4523@gmail.com>
2024-07-12 20:03:32 +0530

    chore: Add trending-blogs section for retrieving popular blogs

--- client/src/pages/home.page.jsx
+++ client/src/pages/home.page.jsx
@@ -2,55 +2,139 @@ import { useEffect, useState } from "react"
 import AnimationWrapper from "../common/page-animation"
 import InPageNavigation from "../components/inpage-navigation.component"
 import Loader from "../components/loader.component"
-import axios from 'axios'
+import axios from "axios"
 import BlogPostCard from "../components/blog-post.component"
+import MinimalBlogPost from "../components/nobanner-blog-post.component"
 
 const HomePage = () => {
+  let [blogs, setBlogs] = useState(null)
+  let [trendingBlogs, setTrendingBlogs] = useState(null)
 
-    let [blogs, setBlogs] = useState(null);
+  let categories = [
+    "programming",
+    "movies",
+    "film making",
+    "politics",
+    "finance",
+    "tech",
+    "travel",
+  ]
 
-    const fetchLatestBlogs = () => {
-        axios.get(import.meta.env.VITE_SERVER_URL + "/latest-blogs")
-        .then(({ data }) => {
-            setBlogs(data.blogs)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }
+  const fetchLatestBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_URL + "/latest-blogs")
+      .then(({ data }) => {
+        setBlogs(data.blogs)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    useEffect(() => {
-        fetchLatestBlogs();
-    }, [])
+  const fetchTrendingBlogs = () => {
+    axios
+      .get(import.meta.env.VITE_SERVER_URL + "/trending-blogs")
+      .then(({ data }) => {
+        setTrendingBlogs(data.blogs)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
 
-    return (
-        <AnimationWrapper>
-            <section className="h-cover flex justify-center gap-10">
-                {/* latest blogs */}
-                <div className="w-full">
-                    <InPageNavigation routes={["home", "trending blogs"]} defaultHidden={["trending blogs"]}>
-                        <>
-                            {
-                                blogs == null ? <Loader /> : 
-                                blogs.map((blog, index) => {
-                                    return <AnimationWrapper transition={{ duration: 1, delay: index*0.1 }} key={index}>
-                                        <BlogPostCard content={blog} author={blog.author.personal_info} />    
-                                    </AnimationWrapper>
-                                })
-                            }
-                        </>
+  useEffect(() => {
+    fetchLatestBlogs()
+    fetchTrendingBlogs()
+  }, [])
 
-                        <h1>Trending</h1>
-                    </InPageNavigation>
-                </div>
+  return (
+    <AnimationWrapper>
+      <section className="h-cover flex justify-center gap-10">
+        {/* latest blogs */}
+        <div className="w-full">
+          <InPageNavigation
+            routes={["home", "trending blogs"]}
+            defaultHidden={["trending blogs"]}
+          >
+            <>
+              {blogs == null ? (
+                <Loader />
+              ) : (
+                blogs.map((blog, index) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: index * 0.1 }}
+                      key={index}
+                    >
+                      <BlogPostCard
+                        content={blog}
+                        author={blog.author.personal_info}
+                      />
+                    </AnimationWrapper>
+                  )
+                })
+              )}
+            </>
 
-                {/* filters and trending blogs */}
-                <div>
+            {trendingBlogs == null ? (
+              <Loader />
+            ) : (
+              trendingBlogs.map((blog, index) => {
+                return (
+                  <AnimationWrapper
+                    transition={{ duration: 1, delay: index * 0.1 }}
+                    key={index}
+                  >
+                    <MinimalBlogPost blog={blog} index={index} />
+                  </AnimationWrapper>
+                )
+              })
+            )}
+          </InPageNavigation>
+        </div>
 
-                </div>
-            </section>
-        </AnimationWrapper>
-    )
+        {/* filters and trending blogs */}
+        <div className="min-w-[40%] lg:min-w-[400px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+          <div className="flex flex-col gap-10">
+            <div>
+              <h1 className="font-medium text-xl mb-8">Stories</h1>
+
+              <div className="flex flex-wrap gap-3">
+                {categories.map((category, i) => {
+                  return (
+                    <button key={i} className="tag">
+                      {category}
+                    </button>
+                  )
+                })}
+              </div>
+            </div>
+
+            <div>
+              <h1 className="font-medium text-xl mb-8">
+                Trending <i className="fi fi-rr-arrow-trend-up"></i>
+              </h1>
+
+              {trendingBlogs == null ? (
+                <Loader />
+              ) : (
+                trendingBlogs.map((blog, index) => {
+                  return (
+                    <AnimationWrapper
+                      transition={{ duration: 1, delay: index * 0.1 }}
+                      key={index}
+                    >
+                      <MinimalBlogPost blog={blog} index={index} />
+                    </AnimationWrapper>
+                  )
+                })
+              )}
+            </div>
+          </div>
+        </div>
+      </section>
+    </AnimationWrapper>
+  )
 }
 
 export default HomePage
--- server/server.js
+++ server/server.js
@@ -293,6 +293,22 @@ app.get('/latest-blogs', (req, res) => {
         })
 })
 
+app.get('/trending-blogs', (req, res) => {
+    Blog.find({ draft: false })
+        .populate("author", "personal_info.profile_img personal_info.username personal_info.fullname -_id")
+        .sort({ "activity.total_reads": -1, "activity.total_likes": -1, "publishedAt": -1 })
+        .select("blog_id title publishedAt -_id")
+        .limit(5)
+        .then(blogs => {
+            return res.status(200).json({
+                blogs
+            })
+        })
+        .catch(error => {
+            return res.status(500).json({ error: error.message })
+        })
+})
+
 app.post('/create-blog', verifyJWT, (req, res) => {
     let authorId = req.user;
 
